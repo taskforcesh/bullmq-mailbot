13609472e2c5c39a2554943207a633ee846db12e
Manuel Astudillo <manuel@optimalbits.com>
2021-03-01 11:04:29 +0100

    chore: add send-limited.ts

---
+++ src/tests/send-limited.ts
@@ -0,0 +1,19 @@
+import { MailbotClient } from "../mail.client";
+import config from "../config";
+
+const args = process.argv.slice(2);
+
+const client = new MailbotClient({
+  connection: config.connection,
+});
+
+const NUM_MAILS = 10;
+
+for (let i = 0; i < NUM_MAILS; i++) {
+  client.enqueue("rate-limited", {
+    from: "manast@taskforce.sh",
+    to: args[0],
+    subject: `This is your email #${i}`,
+    text: "You are receiving email #{i}",
+  });
+}
