69f5b58cbd9289ca401a091fbb6718351380b211
Manuel Astudillo <manuel@optimalbits.com>
2021-03-05 13:44:39 +0100

    feat: add smtp support

--- src/config.ts
+++ src/config.ts
@@ -6,4 +6,13 @@ export default {
     port: parseInt(process.env.REDIS_PORT || "6379"),
   },
   region: process.env.AWS_DEFAULT_REGION || "us-west-2",
+  smtp: {
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT ? parseInt(process.env.SMTP_PORT) : void 0,
+    secure: true, // upgrade later with STARTTLS
+    auth: {
+      user: process.env.SMTP_AUTH_USER,
+      pass: process.env.SMTP_AUTH_PASS,
+    },
+  },
 };
